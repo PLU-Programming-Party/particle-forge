4afc5d8003b03d965aa769ff5434f64a3afff18f
hledenko <hledenko@gmail.com>
2024-06-12 10:08:47 -0700

    Update main.js render sphere

--- main.js
+++ main.js
@@ -1,39 +1,53 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // createa a new scene: this holds the objects to be rendered
 const scene = new THREE.Scene();
 
 // create a new perspective: this defines the view of the scene
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 0, 30);
+camera.lookAt(0,0,0);
 
 // create a renderer: which will render the scene
 const renderer = new THREE.WebGLRenderer();
-
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
+// renderer.setAnimationLoop( animate );
 
 // add renderer canvas element to html body so it can be displayed in browser
 document.body.appendChild( renderer.domElement );
 
-// create a box geometry: contains all points of cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
-// create a material object that defines the color of the geometry
-const material = new THREE.MeshBasicMaterial( { color: 0xff69b4 } );
+const geometry = new THREE.SphereGeometry( 10, 32, 32 );
+// const material = new THREE.MeshBasicMaterial( { color: 0xff69b4 } );
+// const sphere = new THREE.Mesh( geometry, material );
+const material = new THREE.PointsMaterial(
+    {
+        color: 'rgb(255, 255, 255)',
+        size: 0.25
+    }
+);
+const sphere = new THREE.Points(geometry, material);
+scene.add( sphere );
 
-// creat a mesh object: takes geometry and applies material to it
-const cube = new THREE.Mesh( geometry, material );
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.target.set(0, 0, 0);
+//orbit.enableRotate = false;
+orbit.update();
+
+const onWindowResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+};
+window.addEventListener("resize", onWindowResize);
 
 
-scene.add( cube );
-
-camera.position.z = 5;
-
+// animation loop
 function animate() {
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
+animate();
